20b24c2f7261b3675ed72e9e5080e99c805f92b3
Mohan <mohan@ramailo.tech>
2024-02-18 11:16:05 +0545

    add expense views

--- app/urls.py
+++ app/urls.py
@@ -11,4 +11,16 @@ urlpatterns = [
         name="get_user_profile_by_username",
     ),
     path('user/<int:id>/delete/', views.delete_user, name="delete_user"),
+    path('expense/create/', views.create_expense, name="create_expense"),
+    path('expense/<int:id>', views.get_expense_by_id, name="get_expense_by_id"),
+    path(
+        'expense/user/<int:id>', views.get_expenses_by_user, name="get_expenses_by_user"
+    ),
+    path('expense/<int:id>/update/', views.update_expense, name="update_expense"),
+    path(
+        'expense/user/<int:id>/total/',
+        views.get_total_amount_spent_by_user,
+        name="get_total_amount_spent_by_user",
+    ),
+    path('expense/<int:id>/delete/', views.delete_expense, name="delete_expense"),
 ]
