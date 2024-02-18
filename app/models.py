6b288f1523c25ed583d8bc94a58570e713b7c368
Mohan <mohan@ramailo.tech>
2024-02-18 11:49:57 +0545

    add super user

--- app/models.py
+++ app/models.py
@@ -1,11 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    AbstractUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.contrib.auth.validators import ASCIIUsernameValidator
 from django.core.validators import validate_email
 
 
+class UserProfileManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **kwargs):
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **kwargs):
+        kwargs.setdefault('is_staff', True)
+        kwargs.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **kwargs)
+
+
 # Custom User
-class UserProfile(AbstractBaseUser):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(
         max_length=150,
         unique=True,
@@ -18,6 +37,11 @@ class UserProfile(AbstractBaseUser):
     )
     date_of_birth = models.DateField(null=True, blank=True)
     USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+    EMAIL_FIELD = "email"
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserProfileManager()
 
 
 class Category(models.Model):
